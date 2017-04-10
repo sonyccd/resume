1baab6c8f28bda21a2e2bc4cc37ffd9a80c1b938
Brad Bazemore <bwbazemore@uga.edu>
2017-04-10 10:00:40 -0400

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,80 @@
+var gulp = require('gulp');
+var csso = require('gulp-csso');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var cp = require('child_process');
+var imagemin = require('gulp-imagemin');
+var browserSync = require('browser-sync');
+
+var jekyllCommand = (/^win/.test(process.platform)) ? 'jekyll.bat' : 'jekyll';
+
+/*
+ * Build the Jekyll Site
+ * runs a child process in node that runs the jekyll commands
+ */
+gulp.task('jekyll-build', function (done) {
+  return cp.spawn(jekyllCommand, ['build'], {stdio: 'inherit'})
+      .on('close', done);
+});
+
+/*
+ * Rebuild Jekyll & reload browserSync
+ */
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+  browserSync.reload();
+});
+
+/*
+ * Build the jekyll site and launch browser-sync
+ */
+gulp.task('browser-sync', ['jekyll-build'], function() {
+  browserSync({
+    server: {
+      baseDir: '_site'
+    }
+  });
+});
+
+/*
+ * Compile and minify sass
+ */
+gulp.task('sass', function() {
+  gulp.src('src/styles/**/*.scss')
+      .pipe(plumber())
+      .pipe(sass())
+      .pipe(csso())
+      .pipe(gulp.dest('assets/css'));
+});
+
+/*
+ * Minify images
+ */
+gulp.task('imagemin', function() {
+  return gulp.src('src/img/**/*.{jpg,png,gif}')
+      .pipe(plumber())
+      .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+      .pipe(gulp.dest('assets/img/'));
+});
+
+/**
+ * modernize, compile and minify js
+ */
+gulp.task('js', function(){
+  return gulp.src('src/js/**/*.js')
+      .pipe(plumber())
+      .pipe(modernizr())
+      .pipe(concat('main.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('assets/js/'))
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/styles/**/*.scss', ['sass']);
+  gulp.watch('src/js/**/*.js', ['js']);
+  gulp.watch('src/img/**/*.{jpg,png,gif}', ['imagemin']);
+  gulp.watch(['*html', '_includes/*html', '_layouts/*.html'], ['jekyll-rebuild']);
+});
+
+gulp.task('default', ['js', 'sass', 'browser-sync', 'watch']);
